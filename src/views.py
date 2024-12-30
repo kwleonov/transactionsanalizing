85fd12b02e97cc67771f61fb2e5455d416cf8a29
Konstantin Leonov <leonovkw@yandex.ru>
2024-12-30 23:32:50 +0300

    added the main_page function in the views module.

--- src/views.py
+++ src/views.py
@@ -1,11 +1,34 @@
 import datetime
+import json
 from collections.abc import Callable
+from typing import TypedDict
 from xml.etree import ElementTree as ET
 
+import pandas as pd
 import requests
 
 INNER = Callable[[datetime.date], dict[str, float] | None]
 OUTER = Callable[[str, datetime.date], float | None]
+CardType = TypedDict('CardType', {
+    'last_digits': str,
+    'total_spent': float,
+    'cashback': float
+})
+TransactionInfo = TypedDict('TransactionInfo', {
+    'greeting': str,
+    'cards': list[CardType],
+})
+
+
+def get_date(date_str: str) -> datetime.date | None:
+    """convert date from str with format '%d.%m.%Y' to dict['date': datetime.date, 'time': datetime.time]"""
+
+    try:
+        date = datetime.datetime.strptime(date_str, "%d.%m.%Y")
+        return date.date()
+    except Exception as e:
+        print(f"get_date was executed with error: {e}")
+    return None
 
 
 def greeting(time: datetime.time) -> str:
@@ -90,3 +113,113 @@ def get_currency_rates_by_cbr(date: datetime.date) -> dict[str, float] | None:
         return None
 
     return currency_rates
+
+
+def mask_card(card_number: str) -> str:
+    """masking card number by template 'XXXX',
+    where XXXX is last 4 digits of the card number"""
+
+    last_digits = card_number[-4:]
+
+    return last_digits
+
+
+def read_excel(filename: str) -> pd.DataFrame:
+    """reading transactions data from Excel file 'filename' and
+    return pandas DataFrame or empty data if it was executed with errors."""
+
+    excel_data = pd.DataFrame()
+    try:
+        with open(filename, 'rb') as excel_file:
+            excel_data = pd.read_excel(excel_file)
+            return excel_data
+    except Exception as e:
+        print(f"read_excel() was executed with error: {e}")
+    return excel_data
+
+
+def exchange(amount: float, currency_code: str, date_str: str, get_currency_rate: OUTER) -> float | None:
+    """exchange amount form currency code to RUB"""
+
+    if currency_code == "RUB":
+        return amount
+    date = get_date(date_str)
+    if date is None:
+        print("exchange was executed with error: invalid date format")
+        return None
+    rate = get_currency_rate(currency_code, date)
+    if rate is None:
+        print("exchange was executed with error: get_currency_rate was returned None")
+        return None
+    amount_rub = rate * amount
+    return amount_rub
+
+
+def get_cards_info(df: pd.DataFrame, date: datetime.date, get_currency_rate: OUTER) -> list[CardType]:
+    """getting list of number card and total spent"""
+
+    cards: list[CardType] = list()
+    try:
+        date_end = date.strftime('%d.%m.%Y')
+        date_start = date.strftime('01.%m.%Y')
+        transactions_data = df.loc[
+            (df['Дата платежа'] >= date_start) &
+            (df['Дата платежа'] <= date_end) &
+            (df['Сумма платежа'] < 0) &
+            (df['Статус'] == 'OK'),
+            ['Номер карты', 'Сумма платежа', 'Валюта платежа', 'Дата платежа', 'Кэшбэк']].copy()
+
+        transactions_data['amount'] = transactions_data.apply(lambda x: exchange(
+            x['Сумма платежа'],
+            x['Валюта платежа'],
+            x['Дата платежа'],
+            get_currency_rate), axis=1)
+        transactions_data['cashback'] = transactions_data.apply(lambda x: exchange(
+            x['Кэшбэк'],
+            x['Валюта платежа'],
+            x['Дата платежа'],
+            get_currency_rate), axis=1)
+        grouped = transactions_data[['Номер карты', 'amount', 'cashback']].groupby('Номер карты')
+        cards_sum = grouped.sum().to_dict('index')
+        for k, v in cards_sum.items():
+            cards.append({'last_digits': mask_card(str(k)),
+                          'total_spent': -v['amount'],
+                          'cashback': v['cashback']})
+    except Exception as e:
+        print(f"get_cards_info was executed with error: {e}")
+    return cards
+
+
+def main_page(date_str: str = "") -> str:
+    """get date by str with format 'YYYY-MM-DD HH:MM:SS'
+    returns json data:
+    {
+        "greeting": "Добрый день",
+        "cards": [
+            {
+                "last_digits": "1234",
+                "total_spent": 1000.0,
+                "cashback": 100.0,
+            },
+        ]
+    }"""
+
+    json_str = "{}"
+    date_now = datetime.datetime.now()
+    date = date_now.date()
+
+    try:
+        if date_str != "":
+            date = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S").date()
+        json_data: TransactionInfo = dict()  # type: ignore
+        json_data["greeting"] = greeting(date_now.time())
+        df = pd.DataFrame()
+        with open('data/operations.xlsx', 'rb') as f:
+            df = pd.read_excel(f)
+        json_data["cards"] = get_cards_info(df, date, get_currency_rates_by_cbr)
+
+        json_str = json.dumps(json_data, indent=4, ensure_ascii=False)
+    except Exception as e:
+        print(f"main_page was executed with error: {e}")
+
+    return json_str
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,23 @@
 import datetime
+import json
+from collections.abc import Callable
+from typing import TypedDict
+from unittest.mock import Mock, patch
 
+import pandas as pd
 import pytest
 
-from src.views import greeting
+from src.views import (exchange, get_cards_info, get_currency_rates,
+                       get_currency_rates_by_cbr, get_date, greeting,
+                       main_page, mask_card, read_excel)
+
+INNER = Callable[[datetime.date], dict[str, float] | None]
+OUTER = Callable[[str, datetime.date], float | None]
+CardType = TypedDict('CardType', {
+    'last_digits': str,
+    'total_spent': float,
+    'cashback': float
+})
 
 
 @pytest.mark.parametrize("time_int, greeting_str", [
@@ -16,3 +31,195 @@ def test_greeting(time_int: int, greeting_str: str) -> None:
 
     time = datetime.time(hour=time_int)
     assert greeting(time) == greeting_str
+
+
+@pytest.mark.parametrize("date_str, date", [
+    ("30.12.2024", datetime.date(year=2024, month=12, day=30)),
+    ("29.02.2001", None),
+    ("31.06.2000", None),
+    ("01.21.1999", None),
+])
+def test_get_date(date_str: str, date: datetime.date) -> None:
+    """testing convert date from str to datetime.date"""
+
+    assert date == get_date(date_str)
+
+
+@pytest.mark.parametrize("date, currency, rate", [
+    (datetime.date(day=30, month=12, year=2024), ['USD', 'EUR', "CYN"], [1.0, 1.0, None]),
+    (datetime.date(day=10, month=12, year=2024), ['CYN', 'EUR', "USD"], [None, 2.0, 2.0]),
+    (datetime.date(day=17, month=12, year=2024), ['CYN', 'EUR', "USD"], [None, None, None]),
+])
+def test_get_currency_rates(date: datetime.date, currency: list[str], rate: list[float | None]) -> None:
+    """testing decorator"""
+
+    def f(date: datetime.date) -> dict[str, float] | None:
+        """function for testing decorator"""
+
+        if date.day < 16:
+            return {'USD': 2.0, 'EUR': 2.0}
+
+        if date.day < 20:
+            return None
+
+        return {'USD': 1.0, 'EUR': 1.0}
+
+    get_rate = get_currency_rates(f)
+    for index in range(len(currency)):
+        assert get_rate(currency[index], date) == rate[index]
+
+
+def test_get_currency_rates_by_cbr() -> None:
+    """testing get currency rate by cbr"""
+
+    mock_response = """
+        <ValCurse>
+        <Valute>
+            <CharCode>USD</CharCode>
+            <VunitRate>1,0</VunitRate>
+        </Valute>
+        </ValCurse>
+    """
+
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.content = mock_response
+        mock_get.return_value.status_code = 200
+        result = get_currency_rates_by_cbr('USD', datetime.datetime.now().date())
+        assert result == 1.0
+
+
+def test_bad_xml_data() -> None:
+    """testing get_currency_rates_by_cbr for getting bad xml data"""
+
+    mock_response = """
+            <ValCurse>
+            <Valute>
+                <CharCode>USD</CharCode>
+            </Valute>
+            </ValCurs>
+        """
+
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.content = mock_response
+        mock_get.return_value.status_code = 200
+        date = datetime.datetime.today()
+        result = get_currency_rates_by_cbr('USD', date.replace(day=date.day-1))
+        assert result is None
+
+
+def test_bad_xml_rate() -> None:
+    """testing get_currency_rates_by_cbr for convert bad xml data to float"""
+
+    mock_response = """
+            <ValCurse>
+            <Valute>
+                <CharCode>USD</CharCode>
+                <VunitRate>$1</VunitRate>
+            </Valute>
+            </ValCurse>
+        """
+
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.content = mock_response
+        mock_get.return_value.status_code = 200
+        date = datetime.datetime.today()
+        result = get_currency_rates_by_cbr('USD', date.replace(day=date.day-2))
+        assert result is None
+
+
+def test_mask_card() -> None:
+    """testing mask_card"""
+
+    assert mask_card('*1234') == '1234'
+
+
+@patch('pandas.read_excel')
+def test_read_excel(mock_read: Mock) -> None:
+    """testing read excel file"""
+
+    excel_data = pd.DataFrame([(1, 2)], columns=['a', 'b'])
+    mock_read.return_value = excel_data
+    read_data = read_excel('data/operations.xlsx')
+    assert read_data.equals(excel_data)
+
+
+def test_not_exist_excel() -> None:
+    """testing get not exist excel file"""
+
+    not_exist_excel = read_excel('notexist.xlsx')
+    assert not_exist_excel.empty
+
+
+@pytest.mark.parametrize("amount, currency_code, date_str, func, result", [
+    (100.0, 'RUB', '01.02.2003', lambda x, y: 1.0, 100.0),
+    (100.0, 'USD', '02.02.2003', lambda x, y: 2.0, 200.0),
+    (100.0, 'USD', '29.02.2003', lambda x, y: 3.0, None),
+    (100.0, 'USD', '01.02.2003', lambda x, y: None, None),
+])
+def test_exchange(amount: float, currency_code: str, date_str: str,
+                  func: OUTER, result: float) -> None:
+    """testing exchange"""
+
+    result_amount = exchange(amount, currency_code, date_str, func)
+    assert result_amount is None or result_amount == result
+
+
+def test_get_cards_info() -> None:
+    """testing get_cards_info"""
+
+    date = datetime.date.today()
+    df = pd.DataFrame([
+        ("OK", "*1234", -1000.0, "RUB", "15.12.2024", 100.0),
+        ("OK", "*1234", -1000.0, "RUB", "16.12.2024", 100.0),
+        ("OK", "*1235", -1000.0, "RUB", "15.12.2024", 100.0),
+        ],
+        columns=['Статус', 'Номер карты', 'Сумма платежа', 'Валюта платежа', 'Дата платежа', 'Кэшбэк'])
+    cards = [{
+        "last_digits": "1234",
+        "total_spent": 2000.0,
+        "cashback": 200.0,
+        },
+        {
+            "last_digits": "1235",
+            "total_spent": 1000.0,
+            "cashback": 100.0,
+        },
+    ]
+
+    assert get_cards_info(df, date, lambda x, y: 1.0) == cards
+
+
+def test_get_cards_info_error() -> None:
+    """testing get_cards_info with bad data"""
+
+    date = datetime.date.today()
+    df = pd.DataFrame([
+        ("OK", "*1234", -1000.0, "XYZ", "15.12.2024", 100.0),
+        ("OK", "*1234", -1000.0, "RUB", "16.12.2024", 100.0),
+        ("OK", "*1235", -1000.0, "RUB", "15.12.2024", 100.0),
+    ],
+        columns=['Статус', 'Номер карт', 'Сумма платежа', 'Валюта платежа', 'Дата платежа', 'Кэшбэк'])
+    cards: list[CardType] = []
+    assert get_cards_info(df, date, lambda x, y: None) == cards
+
+
+@pytest.mark.parametrize("date, json_result", [
+    ("2024-12-30 21:58:00",
+     '{"greeting": "", "cards": [{"last_digits": "1234", "total_spent": 2000.0, "cashback": 200.0}]}'),
+    ("2024-12-30 24:00:01", "{}"),
+])
+def test_main_page(date: str, json_result: str) -> None:
+    """testing main_page"""
+
+    with patch('pandas.read_excel') as mock_excel:
+        mock_excel.return_value = pd.DataFrame([
+            ("OK", "*1234", -1000.0, "RUB", "15.12.2024", 100.0),
+            ("OK", "*1234", -1000.0, "RUB", "16.12.2024", 100.0),
+            ],
+            columns=['Статус', 'Номер карты', 'Сумма платежа', 'Валюта платежа', 'Дата платежа', 'Кэшбэк'])
+        json_data = json.loads(json_result)
+        if "greeting" in json_data:
+            json_data["greeting"] = greeting(datetime.datetime.now().time())
+        json_str = json.dumps(json_data, ensure_ascii=False).replace(' ', '')
+        result = main_page(date).replace(' ', '').replace('\n', '')
+        assert result == json_str
