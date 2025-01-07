c31de864e26dd7c80065b306e7e4986e0634da17
Konstantin Leonov <leonovkw@yandex.ru>
2025-01-07 21:27:04 +0300

    added logging the utils module.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 # the utils module
 
+import logging
+from os import makedirs
+
 import pandas as pd
+
+log_file = "logs/utils.log"
+log_ok_str = "was executed without errors"
+makedirs("logs", exist_ok=True)
+logger = logging.getLogger(__name__)
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler = logging.FileHandler(log_file, mode="w")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def read_excel(filename: str) -> pd.DataFrame:
@@ -11,7 +24,9 @@ def read_excel(filename: str) -> pd.DataFrame:
     try:
         with open(filename, "rb") as excel_file:
             excel_data = pd.read_excel(excel_file)
-            return excel_data
     except Exception as e:
-        print(f"read_excel() was executed with error: {e}")
+        logger.error(f"read_excel() was executed with error: {e}")
+        return excel_data
+
+    logger.debug(f"read_excel {log_ok_str}")
     return excel_data
